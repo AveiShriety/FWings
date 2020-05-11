5670a32220bb1bfe27dadececd623d03bd432d70
aveishriety <aveishriety@outlook.com>
2020-05-11 10:28:11 +0800

    新增springboot初始化项目

---
+++ src/main/java/com/avei/shriety/FWingsApplication.java
@@ -0,0 +1,13 @@
+package com.avei.shriety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FWingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FWingsApplication.class, args);
+	}
+
+}
